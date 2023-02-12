95702b458b3000824ecc70fd84cafe83f9cb839b
Your Name <you@example.com>
2023-02-12 21:38:59 +0300

    zadacha_003

---
+++ Zadacha_002/Program.cs
@@ -0,0 +1,15 @@
+﻿// Подсчитать сумму цифр в числе
+
+
+int n = new Random().Next(1, 100000);
+int sumDigit = 0;
+Console.Write($"Сумма цифр числа {n} равна ");
+
+while (n > 0)
+{
+    int digit = n % 10;
+    sumDigit = sumDigit + digit;
+    n = n / 10;
+}
+
+Console.WriteLine(sumDigit);
